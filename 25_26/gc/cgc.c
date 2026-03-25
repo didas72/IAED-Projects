6e427df32e26e53116226ce3b81aa1ce0d897175
didas72 <diogocruzdiniz@gmail.com>
2026-03-25 22:50:30 +0000

    chore: Cleanups

--- 25_26/gc/cgc.c
+++ 25_26/gc/cgc.c
@@ -5,26 +5,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+// === Internal state ===
+
 //hashtable_t<void*, size_t>
 static hashtable_t *allocs = NULL;
+static void *stack_base = NULL;
 
-static void cgc_cleanup();
-static void cgc_init()
-{
-	allocs = hashtable_create(hash_ptr, compare_ptr);
-	atexit(cgc_cleanup);
-}
 
-static void cgc_cleanup()
-{
-	//Run normal free to cleanup left-over allocs
-	hashtable_destroy_free(allocs, free, NULL);
-}
+
+// === Private declarations ===
+
+static void _init();
+static void _cleanup();
+static void _inner_free(void *ptr);
+static void *get_stack_base();
+
+static void _collect();
+static void _mark(hashset_t *marked, void *stack_pointer);
+static void _sweep(hashset_t *marked);
+
+
+
+// === Public implementations ===
 
 void *cgc_malloc(size_t size)
 {
 	if (allocs == NULL)
-		cgc_init();
+		_init();
 
 	void *ptr = malloc(size);
 
@@ -33,36 +42,66 @@ void *cgc_malloc(size_t size)
 	return ptr;
 }
 
+void cgc_collect()
+{
+	_collect();
+}
+
+void *cgc_free(void *ptr)
+{
+	if (allocs == NULL)
+		_init();
+
+	_inner_free(ptr);
+}
+
+
+
+// === Private implementations ===
+
+static void _init()
+{
+	allocs = hashtable_create(hash_ptr, compare_ptr);
+	stack_base = get_stack_base();
+	atexit(_cleanup);
+}
+
+static void _cleanup()
+{
+	//Run normal free to cleanup left-over allocs
+	hashtable_destroy_free(allocs, free, NULL);
+}
+
 static void _inner_free(void *ptr)
 {
 	hashtable_remove(allocs, ptr, NULL, NULL);
 	free(ptr);
 }
 
-void *cgc_free(void *ptr)
-{
-	if (allocs == NULL)
-		cgc_init();
-
-	_inner_free(ptr);
-}
-
 #define get_sp() __builtin_frame_address(0)
-static void *get_stack_base();
+
+static void _collect()
+{
+	//hashset_t<void*>
+	hashset_t *marked = hashset_create(hash_ptr, compare_ptr);
+
+	void *stack_pointer = get_sp();
+	_mark(marked, stack_pointer);
+	_sweep(marked);
+
+	hashset_destroy(marked);
+}
 
 /// @brief Populates marked with the allocs found to be referenced
 /// @param marked hashset_t<void*>
-static void cgc_mark(hashset_t *marked, void *stack_pointer)
+static void _mark(hashset_t *marked, void *stack_pointer)
 {
-	void *stack_base = get_stack_base();
-
-	uint8_t *alloc_base;
-	size_t alloc_size;
-
 	//ivector_t<void*>
 	ivector_t *ptrs = hashtable_list_keys(allocs);
+	void **ptr_v = ivector_as_pointer(ptrs);
 	//ivector_t<size_t>
 	ivector_t *sizes = hashtable_list_contents(allocs);
+	size_t *size_v = ivector_as_pointer(sizes);
 
 	size_t alloc_count = ivector_get_count(ptrs);
 
@@ -73,8 +112,8 @@ static void cgc_mark(hashset_t *marked, void *stack_pointer)
 
 		for (size_t i = 0; i < alloc_count; ++i)
 		{
-			ivector_fetch(ptrs, i, &alloc_base);
-			ivector_fetch(sizes, i, &alloc_size);
+			uint8_t *alloc_base = ptr_v[i];
+			size_t alloc_size = size_v[i];
 
 			if (ptr < (void*)alloc_base) continue;
 			if (ptr > (void*)(alloc_base+alloc_size)) continue;
@@ -92,7 +131,7 @@ static void cgc_mark(hashset_t *marked, void *stack_pointer)
 }
 
 /// @param marked hashset_t<void*>
-static void cgc_sweep(hashset_t *marked)
+static void _sweep(hashset_t *marked)
 {
 	//ivector_t<void *>
 	ivector_t *all_allocs = hashtable_list_keys(allocs);
@@ -110,18 +149,6 @@ static void cgc_sweep(hashset_t *marked)
 	}
 
 	ivector_destroy(all_allocs);
-}
-
-void cgc_collect()
-{
-	//hashset_t<void*>
-	hashset_t *marked = hashset_create(hash_ptr, compare_ptr);
-
-	void *stack_pointer = get_sp();
-	cgc_mark(marked, stack_pointer);
-	cgc_sweep(marked);
-
-	hashset_destroy(marked);
 }
 
 #define __USE_GNU
